5adf7731a145108010998b5890b8f31ff3dfedd9
Will Luna <william.luna@rappi.com>
2023-12-27 12:21:23 -0500

    Curso Material UI #03: Typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import { Button, Container } from "@mui/material";
+import {Button, Container, Typography} from  "@mui/material"
 
-export default function App(){
+
+export default function App() {
     return(
-      <Container sx={{ border: 3, boxShadow: 3, pb: 2}}>
-        <h1>App</h1>
-        <Button variant="contained">Mi primer Boton</Button>
-      </Container>
-    )
+      <Container sx={{border: 3, boxShadow: 3, pb: 2}}>
+      <h1>App</h1>
+      <Typography 
+      variant="h1"
+      component="h2"
+      
+      >
+        Titulo 1
+      </Typography>
+      <Typography variant="h1">Titulo 2</Typography>
+      <Typography
+        variant="body1"
+        textAlign="center"
+        mt={50}
+      >
+          Titulo 3
+        </Typography>
+    <Button variant="contained">Mi primer botón</Button>
+    </Container>
+    );
 }
