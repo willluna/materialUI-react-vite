56d02745672547aab5c92988feeceea2e5d40dc4
Will Luna <william.luna@rappi.com>
2023-12-28 16:46:37 -0500

    Curso Material UI #11: Navbar responsive (Parte #02) Drawer component

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,9 +1,23 @@
+import { Button, Drawer } from "@mui/material";
+import { useState } from "react";
 import NavListDrawer from "./NavListDrawer";
 
+
 export default function Navbar(){
+    
+    const [open, setOpen] = useState(false)
+
     return(
         <>
+            <Button variant="contained" onClick={() => setOpen(true)}>Open Drawer</Button>
+
+            <Drawer
+                open={open}
+                anchor="left"
+                onClose={() => setOpen(false)}
+            >
             <NavListDrawer/>
+            </Drawer> 
         </>
     )
 }
