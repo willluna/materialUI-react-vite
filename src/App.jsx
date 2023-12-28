746ade57f985eaf9bc4a6826270af2a22eed0715
Will Luna <william.luna@rappi.com>
2023-12-28 13:49:06 -0500

    Curso Material UI #08: Práctica con styled (@emotion/styled), Box y Flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,12 @@
-import { Container, Grid } from  "@mui/material"
+import { Container} from  "@mui/material"
+import Product from "./Product";
 
 
 
 export default function App() {
     return(
       <Container>
-        <Grid container spacing={5}>
-          <Grid
-            item 
-            xs={12}
-            sm={6}
-            md={4}
-          >
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti esse illum quia sed, eum necessitatibus ipsa. Aperiam exercitationem recusandae quis dicta aliquid incidunt aliquam numquam eum rerum molestias, vel voluptatem!
-            </p>
-          </Grid>
-          <Grid 
-            item 
-            xs={12}
-            sm={6}
-            md={4}
-          >
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti esse illum quia sed, eum necessitatibus ipsa. Aperiam exercitationem recusandae quis dicta aliquid incidunt aliquam numquam eum rerum molestias, vel voluptatem!
-            </p>
-          </Grid>
-          <Grid 
-            item 
-            xs={12}
-            sm={12}
-            md={4}
-          >
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti esse illum quia sed, eum necessitatibus ipsa. Aperiam exercitationem recusandae quis dicta aliquid incidunt aliquam numquam eum rerum molestias, vel voluptatem!
-            </p>
-          </Grid>
-        </Grid>
+        <Product/>
       </Container>
     );
 }
